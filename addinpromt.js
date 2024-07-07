7beb2fff360464e4c65bcc9a7745a479b609a7bd
Victor Kibet <victorkibet2012@gmail.com>
2024-07-07 15:09:51 +0300

    Create addinpromt.js

---
+++ addinpromt.js
@@ -0,0 +1,8 @@
+//writting javascript thats taks two user input and its give out put
+// its work only in browser
+// store input numbers
+const num1 = parseInt(prompt('Enter the fast number '));
+const num2 = parseInt(prompt('Enter the second number'));
+//add two numbers
+const sum = num1 + num2;
+console.log(`the sum of ${num1} and ${num2} is ${sum}`);
